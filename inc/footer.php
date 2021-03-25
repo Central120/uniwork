141685fdeb60178d7e6d0461ad9b9f5726c17430
shaunlawlor <shaun.lawlor5@gmail.com>
2021-03-25 21:08:07 +0000

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,4 +1,4 @@
-<footer class='footer bg-dark text-white' style='padding-top: 0px;left:0;bottom:0;width:100%;'>
+<footer class='footer bg-dark text-white' style='padding-top: 0px;left:0;bottom:0;width:100%;position:absolute;'>
 
 <!-- Social Icons -->
 <div class='bg-primary'>
