096e7a3ec49ad6b5679d7fcdcec04ada373ea658
Central120 <81374520+Central120@users.noreply.github.com>
2021-03-25 15:19:42 +0000

    Create dbconnect.php

---
+++ inc/dbconnect.php
@@ -0,0 +1,15 @@
+<?php
+
+$serverhost = "localhost";
+$username = "u898383871_root";
+$password = "DatabasePassword123!";
+$dbname = "u898383871_uni";
+
+$con = mysqli_connect("$serverhost", "$username", "$password", "$dbname");
+
+// Check connection
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
+}
+?>
