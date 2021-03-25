f31ca262d227d2ed13e395e74af4a443e81448ee
shaunlawlor <shaun.lawlor5@gmail.com>
2021-03-25 17:40:27 +0000

    Header testing

--- index.php
+++ index.php
@@ -3,3 +3,13 @@
 echo "hi";
 
 ?>
+<html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+</head>
+<body>
+<?php include "inc/header.php"; ?>
+
+</body>
+</html>
