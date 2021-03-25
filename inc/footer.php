b86422d2412568fe0f7fa9b46efec0fdeeeb6725
shaunlawlor <shaun.lawlor5@gmail.com>
2021-03-25 20:37:53 +0000

    footer + index update

---
+++ inc/footer.php
@@ -0,0 +1,75 @@
+<footer class='footer bg-dark text-white' style='padding-top: 0px'>
+
+<!-- Social Icons -->
+<div class='bg-primary'>
+    <div class='container'>
+        <div class='row py-4'>
+            <div class='col-md-6 col-lg-7'>
+                <h4 class='mb-0 white-text'>Follow us on all major social networking platforms with the tag @KerrysK9s</h4>
+            </div>
+            <div class='col-md-6 col-lg-5 text-right'>
+                <a><i class='fa fa-facebook white-text mr-lg-4 fa-2x'> </i></a>
+                <a><i class='fa fa-twitter white-text mr-lg-4 fa-2x'> </i></a>
+                <a><i class='fa fa-google-plus white-text mr-lg-4 fa-2x'> </i></a>
+                <a><i class='fa fa-linkedin white-text mr-lg-4 fa-2x'> </i></a>
+                <a><i class='fa fa-instagram white-text mr-lg-4 fa-2x'> </i></a>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Social Icons -->
+
+<!-- Footer Links -->
+<div class='container pt-5 pb-2'>
+    <div class='row'>
+
+        <div class='col-md-3 col-lg-4 col-xl-3'>
+            <h4>Our Promise</h4>
+            <hr class='bg-white mb-2 mt-0 d-inline-block mx-auto w-25'>
+            <p>
+               Here at Kerry's K9's, we like to give you, the customer the best experience we can. Therefore, please email us with any suggestions or improvements we can do for you! 
+            </p>
+        </div>
+
+        <div class='col-md-2 col-lg-2 col-xl-2 mx-auto'>
+            <h4>Popular Items</h4>
+            <hr class='bg-white mb-2 mt-0 d-inline-block mx-auto w-25'>
+            <p><a href='#' class='text-white'>#</a></p>
+            <p><a href='#' class='text-white'>#</a></p>
+            <p><a href='#' class='text-white'>#</a></p>
+            <p><a href='#' class='text-white'>#</a></p>
+        </div>
+
+        <div class='col-md-3 col-lg-2 col-xl-2 mx-auto'>
+            <h4>Links</h4>
+            <hr class='bg-white mb-2 mt-0 d-inline-block mx-auto w-25'>
+            <p><a href='#' class='text-white'>#</a></p>
+            <p><a href='#' class='text-white'>#</a></p>
+            <p><a href='#' class='text-white'>#</a></p>
+            <p><a href='#' class='text-white'>#</a></p>
+        </div>
+
+        <div class='col-md-4 col-lg-3 col-xl-3'>
+            <h4>Contact</h4>
+            <hr class='bg-white mb-2 mt-0 d-inline-block mx-auto w-25'>
+            <p><i class='fa fa-home mr-3'></i> #</p>
+            <p><i class='fa fa-envelope mr-3'></i>#</p>
+            <p><i class='fa fa-phone mr-3'></i> #</p>
+            <p><i class='fa fa-print mr-3'></i> #</p>
+        </div>
+
+    </div>
+</div>
+<!-- Footer Links-->
+
+<hr class='bg-white mx-4 mt-2 mb-1'>
+
+<!-- Copyright-->
+<div class='container-fluid'>
+    <p class='text-center m-0 py-1'>
+        © 2021 Copyright <a href='index.php' class='text-white'>Kerry's K9's</a>
+    </p>
+</div>
+<!-- Copyright -->
+
+</footer>
--- index.php
+++ index.php
@@ -1,15 +1,25 @@
-<?php
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>YMCA Caf&eacute; - Home</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-echo "hi";
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css'>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="custom-css/style1.css">
+		
+		
+  </head>
+  <body>
+  <?php include "inc/header.php"; ?>
+  <h2 class="mb-4">Welcome to Kerry's K9's!</h2>
+  <p>Below are the most recent announcements.</p>
+  <br>
+  <p>We currently discounts on the following items: </p>
 
-?>
-<html>
-<head>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-</head>
-<body>
-<?php include "inc/header.php"; ?>
+  <?php include "inc/footer.php"; ?>
 
 </body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
