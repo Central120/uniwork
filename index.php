5e507e93bfdcfdd1288a771077b15a62a027213d
shaunlawlor <shaun.lawlor5@gmail.com>
2021-03-25 20:44:29 +0000

    Update index.php
    same issue as before

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
   	<title>Kerry's K9's - Home</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css'>
-	
+
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
-  
+   	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
 		
   </head>
   <body>
