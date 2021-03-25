e63c8cb9599d12eac836c917211b0e3fad9d5c9d
Central120 <81374520+Central120@users.noreply.github.com>
2021-03-25 15:43:26 +0000

    Update dbconnect.php

--- inc/dbconnect.php
+++ inc/dbconnect.php
@@ -11,6 +11,5 @@ $con = mysqli_connect("$serverhost", "$username", "$password", "$dbname");
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
     exit();
-    echo "test";
 }
 ?>
