61df2e9582168e8523b21c233f46639b5f7ecc7d
shaunlawlor <shaun.lawlor5@gmail.com>
2021-03-26 15:35:37 +0000

    remove .php from end of url

--- inc/header.php
+++ inc/header.php
@@ -4,7 +4,7 @@ $url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
 
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-<a class="navbar-brand" href="#">
+<a class="navbar-brand" href="index">
     <img src="images/logo.png" width="64" height="64" alt="">
   </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -16,7 +16,7 @@ $url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
       <li class="nav-item <?php if (strpos($url,'index') !== false) {
     echo 'active';
 } ?>">
-        <a class="nav-link" href="#">Home</a>
+        <a class="nav-link" href="index">Home</a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -63,7 +63,7 @@ $url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
           <a class="dropdown-item <?php if (strpos($url,'login') !== false) {
     echo 'active';
-} ?>" href="#">Login</a>
+} ?>" href="login">Login</a>
           <a class="dropdown-item <?php if (strpos($url,'signup') !== false) {
     echo 'active';
 } ?>" href="#">Sign up</a>
