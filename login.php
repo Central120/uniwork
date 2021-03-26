dadfa8b60c9f3710a52dc650fcaa92f7674d5171
Central120 <nathanwatters3212@gmail.com>
2021-03-26 12:51:24 +0000

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,28 @@
-<?php
-echo "hi";
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>Kerry's K9's - Account Login</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+   	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css' />
 
-?>
+		
+  </head>
+  <body>
+  <?php include "inc/header.php"; ?>
+<div class="container">
+    <center>
+        <h2 class="mb-42">Kerry's K9 - Account Login</h2>
+    </center>
+</div>
+
+
+
+<?php include "inc/footer.php"; ?>
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+</html>
