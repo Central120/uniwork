5961a462f1a47a87fbd0a6e2e9fb2ca6960c3bc8
shaunlawlor <shaun.lawlor5@gmail.com>
2021-03-26 19:47:06 +0000

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,4 +1,4 @@
-<footer class='footer bg-dark text-white' style='padding-top: 0px;left:0;bottom:0;width:100%;position:absolute;'>
+<footer class='footer bg-dark text-white' style='padding-top: 0px;left:0;bottom:-50vh;width:100%;'>
 
 <?php
     $startYear = "2019";
