11b08676f909185ee0dd5aa22b2c1a3c7d2cc649
Central120 <nathanwatters3212@gmail.com>
2021-03-26 12:30:22 +0000

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 <a class="navbar-brand" href="#">
-    <img src="images/logo.png" width="30" height="30" alt="">
+    <img src="images/logo.png" width="64" height="64" alt="">
   </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
