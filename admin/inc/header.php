5a79b0e53f0a4358d89527260125e0a6b310b849
shaunlawlor <shaun.lawlor5@gmail.com>
2021-03-27 16:47:49 +0000

    Update header.php

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -14,7 +14,7 @@ $url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 <a class="navbar-brand" href="index">
-    <img src="images/logo.png" width="64" height="64" alt="">
+    <img src="../images/logo.png" width="64" height="64" alt="">
   </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
