494977fd4705384b02befbaa293a8a676504462b
shaunlawlor <shaun.lawlor5@gmail.com>
2021-04-01 18:15:15 +0100

    Update new-booking.php

--- new-booking.php
+++ new-booking.php
@@ -141,7 +141,7 @@ if ($countfindavail == 0)
       <label for="inputEmail4">Preference Two - Date</label>
      <select class="form-control" name="">
      <?php
-     $today = strtotime('this sunday');
+     $today = strtotime('+2 days');
      if ($today > $findldate)
         {
           $findfdate = strtotime("next week $last_day");
