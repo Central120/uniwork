646fd79450c321b58bf6e67609d61fc39e5b9a1e
shaunlawlor <shaun.lawlor5@gmail.com>
2021-04-01 02:12:04 +0100

    Update new-booking.php

--- new-booking.php
+++ new-booking.php
@@ -24,7 +24,7 @@ $countfindavail = mysqli_num_rows($findavail);
 
 if ($countfindavail == 0)
 {
-  $msg = "<br><br>$staff_name is currently unavailable";
+  $msg = "<br><br><h2 style='margin-left:45%'>$staff_name is currently unavailable - Please try again later.</h2>";
   
 }
 
