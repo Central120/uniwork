e228944114d75cf5351608e380654d9cd9ab86b7
shaunlawlor <shaun.lawlor5@gmail.com>
2021-04-01 02:01:30 +0100

    Update new-booking.php

--- new-booking.php
+++ new-booking.php
@@ -131,8 +131,5 @@ if ($countfindavail == 0)
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 </html>
-<?php 
 
-  }
-
-  ?>
+  
