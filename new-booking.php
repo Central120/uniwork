c69dc116675bf163f9fa24ec024ad3efed4e4942
shaunlawlor <shaun.lawlor5@gmail.com>
2021-04-02 23:17:30 +0100

    fixed error - uncommenting out ajax call

--- new-booking.php
+++ new-booking.php
@@ -260,7 +260,7 @@ if ($countfindavail == 0)
 
     <?php include "inc/footer.php"; ?>
 </body>
-<!-- <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
   <script type='text/javascript'>
     $('#Form1').submit(function(event) {
       event.preventDefault(); //prevent default action
@@ -276,7 +276,7 @@ if ($countfindavail == 0)
 
       });
     });
-  </script> -->
+  </script>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
