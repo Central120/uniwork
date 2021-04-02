a73c03785a51d829fc547a16f3153202a36315ef
WhySoSerious4 <80232423+WhySoSerious4@users.noreply.github.com>
2021-04-02 22:45:28 +0100

    Create Shop.php

---
+++ Shop.php
@@ -0,0 +1,35 @@
+<?php
+include_once 'inc/dbconnect.php';
+session_start();
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>Shop</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+   	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css' />
+
+		
+  </head>
+  <body>
+  <?php include "inc/header.php"; ?>
+  <div class="container-fluid" style='min-height:40vh'>
+  <h2 class="mb-4">Welcome to Kerry's K9's!</h2>
+  <p>Below are the most recent announcements.</p>
+  <br>
+  <p>We currently discounts on the following items: </p>
+
+</div>
+
+<?php include "inc/footer.php"; ?>
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+</html>
