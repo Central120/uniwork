a7b2472ee5a834a3e449e3abb627f2aab7300572
Central120 <nathanwatters3212@gmail.com>
2021-04-03 17:42:05 +0100

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -257,6 +257,12 @@ $findcurrentbookings = mysqli_query($conn, "SELECT * FROM `bookings` WHERE `user
   <?php include "inc/footer.php"; ?>
 </body>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+
+<script type="text/javascript">
+const myCarousel = document.querySelector('#myCarousel')
+const carousel = new mdb.Carousel(myCarousel)
+</script>
+
   <script type='text/javascript'>
     $('#Form1').submit(function(event) {
       event.preventDefault(); //prevent default action
