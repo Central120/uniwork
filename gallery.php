d694864ab75ff15778c2749e90d59d4d86a7ca44
Central120 <nathanwatters3212@gmail.com>
2021-04-03 17:48:25 +0100

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -83,7 +83,7 @@ $findcurrentbookings = mysqli_query($conn, "SELECT * FROM `bookings` WHERE `user
       <div class="col-xl-3 col-lg-4 col-md-6 mb-4">
         <div class="bg-dark rounded shadow-sm"><img src="https://res.cloudinary.com/mhmd/image/upload/v1556294928/nicole-honeywill-546848-unsplash_ymprvp.jpg" alt="" class="img-fluid card-img-top">
           <div class="p-4">
-            <h5> <a href="#" class="text-dark">And She Realized</a></h5>
+            <h5> <a href="#" class="text-white">And She Realized</a></h5>
             <p class="small text-muted mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
             <div class="d-flex align-items-center justify-content-between rounded-pill bg-light px-3 py-2 mt-4">
               <p class="small mb-0"><i class="fa fa-picture-o mr-2"></i><span class="font-weight-bold">JPG</span></p>
