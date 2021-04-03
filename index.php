b6046a9c960050b50ebe60e1a8f32897fdb3cd99
shonsunny <shxnsunny29@gmail.com>
2021-04-03 20:26:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ session_start();
 <!doctype html>
 <html lang="en">
   <head>
-  	<title>Kerry's K9's - Home</title>
+  	<title>Kerry's K9's - Home1</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
