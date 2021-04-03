adb47822a1e098d9be4f843609efeb2cb7b2eb1d
shaunlawlor <shaun.lawlor5@gmail.com>
2021-04-03 16:16:50 +0100

    redirect does not work on same page - attempting this.

--- php/cancel-booking.php
+++ php/cancel-booking.php
@@ -50,7 +50,7 @@ if ($countfindbooking != 0)
         $deletefrombookings = mysqli_query($conn, "DELETE FROM `bookings` WHERE `id` = '$booking_id'");
         if ($deletedfrombookings)
         {
-            echo "<script>window.location.replace('../current-bookings');</script>";
+            echo "<script>window.location.reload();</script>";
         }
     }   
     else
