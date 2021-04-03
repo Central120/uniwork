f63c9f8580df3a247ac598924e95aa11aa28951f
shaunlawlor <shaun.lawlor5@gmail.com>
2021-04-03 01:35:26 +0100

    silly tired error

--- current-bookings.php
+++ current-bookings.php
@@ -76,7 +76,7 @@ $findcurrentbookings = mysqli_query($conn, "SELECT * FROM `bookings` WHERE `user
         }
         else
         {
-            $approved = "Booking will be handled by: <p color='green'>$approver</p>";
+            $approved = "Booking will be handled by: <p style='color:green'>$approver</p>";
         }
 
         if ($approved_timestamp == $ts1)
