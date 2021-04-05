ac1c74b347b0e227c83a324a8a3553a9619337e2
shaunlawlor <shaun.lawlor5@gmail.com>
2021-04-05 05:39:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ session_start();
   <?php include "inc/header.php"; ?>
   <div class="container" style='min-height:40vh'>
   <div class="d-flex justify-content-center">
-  <h2 class="mb-4">Welcome to Kerry's K9's!</h2>
+  <h2 class="mb-4">Welcome to Kerry's K9's!</h2><br>
   <p>Below are the most recent announcements.</p>
   <br>
   <p>We currently discounts on the following items: </p>
