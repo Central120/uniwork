2b45a0f5cfcfd2c220f8317eea649e5973c45c84
WhySoSerious4 <80232423+WhySoSerious4@users.noreply.github.com>
2021-04-05 21:38:51 +0100

    managing sessions

--- filters.php
+++ filters.php
@@ -1,5 +1,20 @@
 <?php
-include "inc/dbconnect.php";
+include_once 'inc/dbconnect.php';
+session_start();
+$current_timestamp = date('Y-m-d H:i:s');
+
+if (isset($_SESSION['user']))
+{
+    $session_usern = $_SESSION['user'];
+}
+else if(isset($_SESSION['admin']))
+{
+    $session_usern = $_SESSION['admin'];
+}
+else
+{
+    echo "<script>window.location.replace('index');</script>";
+}
 $category1 = mysqli_real_escape_string($conn, $_POST['category']);
 if ($category1 == "all")
 {
--- shop.php
+++ shop.php
@@ -1,6 +1,20 @@
 <?php
 include_once 'inc/dbconnect.php';
 session_start();
+$current_timestamp = date('Y-m-d H:i:s');
+
+if (isset($_SESSION['user']))
+{
+    $session_usern = $_SESSION['user'];
+}
+else if(isset($_SESSION['admin']))
+{
+    $session_usern = $_SESSION['admin'];
+}
+else
+{
+    echo "<script>window.location.replace('index');</script>";
+}
 
 
 ?>
