717b43e9403cb98a60143ea6a76e26d303179484
shaunlawlor <shaun.lawlor5@gmail.com>
2021-04-05 18:58:56 +0100

    Update schedule-update.php

--- admin/php/schedule-update.php
+++ admin/php/schedule-update.php
@@ -28,7 +28,9 @@ if ($countcheckforschedule != 0)
 
     if ($start_date == $end_date || $start_time == $end_time || $sd < $ed)
     {
-        echo "<div class='alert alert-danger alert-dismissable fade show' role='alert'><strong>An error occured.</strong> Please ensure the values you have entered are valid. <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+        $strsd = date('Y:m:d', $sd);
+        $stred = date('Y:m:d', $ed);
+        echo "<div class='alert alert-danger alert-dismissable fade show' role='alert'><strong>An error occured.</strong> Please ensure the values you have entered are valid. $strsd - $stred <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
         <span aria-hidden='true'>&times;</span>
       </button></div>";
     }
