ea3c72fcf858f3fb863b3f7c269e629009ab9451
shaunlawlor <shaun.lawlor5@gmail.com>
2021-04-05 05:36:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ session_start();
   <body>
   <?php include "inc/header.php"; ?>
   <div class="container-fluid" style='min-height:40vh'>
-  <div class="d-flex justify-content-center">
+  <div class="justify-content-center">
   <h2 class="mb-4">Welcome to Kerry's K9's!</h2>
   <p>Below are the most recent announcements.</p>
   <br>
