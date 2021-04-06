e71dfe77a28813c311becfb0d6edf81b57f12719
shaunlawlor <shaun.lawlor5@gmail.com>
2021-04-06 02:37:07 +0100

    Update login.php

--- login.php
+++ login.php
@@ -29,6 +29,7 @@
             </div>
 
             <div class="form-group">
+                <button type='button' id='forgot' class="btn btn-info">Forgotten password</button>
                 <button type="submit" class="btn btn-primary">Login</button>
             </div>
         </form>
@@ -55,7 +56,12 @@
       });
     });
   </script>
+<script>
+$('#forgot').click(function(){
+window.location.replace('forgot-password');
+});
 
+  </script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
