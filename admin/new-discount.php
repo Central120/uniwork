7548c07a7367c754fa8ee3820ba586cdc30b0d4c
WhySoSerious4 <80232423+WhySoSerious4@users.noreply.github.com>
2021-04-10 17:05:17 +0100

    Update new-discount.php

--- admin/new-discount.php
+++ admin/new-discount.php
@@ -87,7 +87,7 @@ else
         <?php 
         for ($i=1;$i<=100;$i++)
         {
-            echo "<option value='$i'>$i</option>";
+            echo "<option value='$i'>$i%</option>";
         }
         ?>
          </select>
