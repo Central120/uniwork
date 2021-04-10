26682a328f3f7147c3dd64ce47ee2b3073f75aa5
WhySoSerious4 <80232423+WhySoSerious4@users.noreply.github.com>
2021-04-10 18:07:24 +0100

    deleting discount page and removing popular items on footer

--- admin/inc/footer.php
+++ admin/inc/footer.php
@@ -36,16 +36,9 @@
             </p>
         </div>
 
-        <div class='col-md-2 col-lg-2 col-xl-2 mx-auto'>
-            <h4>Popular Items</h4>
-            <hr class='bg-white mb-2 mt-0 d-inline-block mx-auto w-25'>
-            <p><a href='#' class='text-white'>#</a></p>
-            <p><a href='#' class='text-white'>#</a></p>
-            <p><a href='#' class='text-white'>#</a></p>
-            <p><a href='#' class='text-white'>#</a></p>
-        </div>
+       
 
-        <div class='col-md-3 col-lg-2 col-xl-2 mx-auto'>
+        <div class='col-md-2 col-lg-2 col-xl-2 mx-auto'>
             <h4>Links</h4>
             <hr class='bg-white mb-2 mt-0 d-inline-block mx-auto w-25'>
             <p><a href='../login' class='text-white'>Login</a></p>
@@ -54,7 +47,7 @@
             <p><a href='../shop' class='text-white'>Store</a></p>
         </div>
 
-        <div class='col-md-4 col-lg-3 col-xl-3'>
+        <div class='col-md-3 col-lg-3 col-xl-3'>
             <h4>Contact</h4>
             <hr class='bg-white mb-2 mt-0 d-inline-block mx-auto w-25'>
             <p><i class='fa fa-home mr-3'></i> Tynemouth Road, Wallsend</p>
---
+++ admin/php/delete-discount.php
@@ -0,0 +1,36 @@
+<?php
+include_once '../inc/dbconnect.php';
+session_start();
+$current_timestamp = date('Y-m-d H:i:s');
+
+if(isset($_SESSION['admin']))
+{
+    $session_usern = $_SESSION['admin'];
+}
+else
+{
+    echo "<script>window.location.replace('../index');</script>";
+}
+
+$product_id = mysqli_real_escape_string($conn, $_POST['id']);
+$sqlfindproduct = "SELECT * FROM `products` WHERE `id` = '$product_id'";
+$findproduct = mysqli_query($conn, $sqlfindproduct);
+$countfindproduct = mysqli_num_rows($findproduct);
+
+if ($countfindproduct != 0){
+    $sqldeletediscount = "UPDATE `products` SET `discount` = '0' WHERE `id` = '$product_id'";
+    $deletediscount = mysqli_query($conn, $sqldeletediscount);
+    if ($deletediscount){
+        echo "<script>window.location.replace('modify-products');</script>";
+    }
+    else{
+        echo "<div class='alert alert-danger alert-dismissable fade show' role='alert'><strong>An error occured.</strong> We could not delete the discount. <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+        <span aria-hidden='true'>&times;</span>
+      </button></div>";
+    }
+}
+else{
+    echo "<div class='alert alert-danger alert-dismissable fade show' role='alert'><strong>An error occured.</strong> We could not find the product. <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+    <span aria-hidden='true'>&times;</span>
+  </button></div>";
+}
