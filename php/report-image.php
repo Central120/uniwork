54eb690e0ec80b88a68b445745c7a2a7fd42523f
Central120 <nathanwatters3212@gmail.com>
2021-04-11 15:55:29 +0100

    Update report-image.php

--- php/report-image.php
+++ php/report-image.php
@@ -12,7 +12,26 @@ else if(isset($_SESSION['admin']))
     $session_usern = $_SESSION['admin'];
 }
 
+$reporter = mysqli_real_escape_string($conn, $_POST['reporter']);
+$reporting = mysqli_real_escape_string($conn, $_POST['reporting']);
+$report_option = mysqli_real_escape_string($conn, $_POST['report_option']);
+$report_information = mysqli_real_escape_string($conn, $_POST['report_information']);
 
+$reportsql = "INSERT INTO image_report VALUES(DEFAULT, '$reporter', '$reporting', $report_option', '$report_information', 'pending', 'pending')";
+$reportresult = mysqli_query($conn, $reportsql);
+
+if($reportrow = mysqli_fetch_array($reportresult))
+{
+    echo "<div class='alert alert-success alert-dismissable fade show' role='alert'><strong>Reported!</strong> Our moderation team will review the report and take the appropriate action. Thank you for the report! <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+    <span aria-hidden='true'>&times;</span>
+    </button></div>";
+}
+else
+{
+    echo "div class='alert alert-danger alert-dismissable fade show' role='alert'><strong>Uhh ohh!</strong> We failed to submit your report. Please try again. If this problem persists, please contact our team as soon as possible. <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+    <span aria-hidden='true'>&times;</span>
+    </button></div>";
+}
 
 
 
