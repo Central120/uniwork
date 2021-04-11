9b78d1f560739d4e99051a061c02027bf8af932c
Central120 <nathanwatters3212@gmail.com>
2021-04-11 15:59:03 +0100

    Update report-image.php

--- php/report-image.php
+++ php/report-image.php
@@ -22,15 +22,11 @@ $reportresult = mysqli_query($conn, $reportsql);
 
 if($reportrow == mysqli_fetch_array($reportresult))
 {
-    echo "<div class='alert alert-success alert-dismissable fade show' role='alert'><strong>Reported!</strong> Our moderation team will review the report and take the appropriate action. Thank you for the report! <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
-    <span aria-hidden='true'>&times;</span>
-    </button></div>";
+    echo "success";
 }
 else
 {
-    echo "<div class='alert alert-danger alert-dismissable fade show' role='alert'><strong>Uhh ohh!</strong> We failed to submit your report. Please try again. If this problem persists, please contact our team as soon as possible. <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
-    <span aria-hidden='true'>&times;</span>
-    </button></div>";
+    echo "failed";
 }
 
 
