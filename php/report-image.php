c44b1c14bc0bdf2bca973b03cdbca7af3a7a9f65
Central120 <nathanwatters3212@gmail.com>
2021-04-11 15:56:56 +0100

    Update report-image.php

--- php/report-image.php
+++ php/report-image.php
@@ -20,7 +20,18 @@ $reportinformation = mysqli_real_escape_string($conn, $_POST['report_information
 $reportsql = "INSERT INTO image_report VALUES(DEFAULT, '$reporter', '$reporting', $reportoption', '$reportinformation', 'pending', 'pending')";
 $reportresult = mysqli_query($conn, $reportsql);
 
-
+if($reportrow == mysqli_fetch_array($reportresult))
+{
+    echo "<div class='alert alert-success alert-dismissable fade show' role='alert'><strong>Reported!</strong> Our moderation team will review the report and take the appropriate action. Thank you for the report! <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+    <span aria-hidden='true'>&times;</span>
+    </button></div>";
+}
+else
+{
+    echo "<div class='alert alert-danger alert-dismissable fade show' role='alert'><strong>Uhh ohh!</strong> We failed to submit your report. Please try again. If this problem persists, please contact our team as soon as possible. <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+    <span aria-hidden='true'>&times;</span>
+    </button></div>";
+}
 
 
 
