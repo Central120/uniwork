8bfb5a8bf43c6590a9e1d43a1788064cbf6ee085
WhySoSerious4 <80232423+WhySoSerious4@users.noreply.github.com>
2021-04-11 02:30:24 +0100

    Update modify-product.php

--- admin/php/modify-product.php
+++ admin/php/modify-product.php
@@ -2,7 +2,6 @@
 include_once '../inc/dbconnect.php';
 session_start();
 $current_timestamp = date('Y-m-d H:i:s');
-ini_set('display_errors', 1);
 
 if(isset($_SESSION['admin']))
 {
@@ -144,8 +143,46 @@ else {
         </button></div>";
     }
 }
-echo $msg;
-echo $msg1; 
+
 ?>
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>Kerry's K9's - Modify product</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+   	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css' />
+     
+     <style>
+    .footer 
+    {
+        margin-top:20% !important;
+    }
+     </style>
+</head>
+<body>
+    <?php include "../inc/header.php"; ?>
+    <div class="container">
+    <div class="d-flex justify-content-center">
+    <?php echo $msg; ?><br>
+    <?php echo $msg1; ?><br>
+    <button id='retry' class='btn btn-warning'>Try again</button>
+    </div>
+    </div>
+    <?php include "../inc/footer.php"; ?>
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script>
+$('#retry').click(function()
+{
+window.location.replace('../modify-products');
+});
+</script>
+</html>
+
 
 
