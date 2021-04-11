2271ff7dd6520f75dcc35c10aab75116407bfcef
Central120 <nathanwatters3212@gmail.com>
2021-04-11 23:18:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -36,6 +36,29 @@ session_start();
     <li data-target="#carousel-example-generic" data-slide-to="1"></li>
     <li data-target="#carousel-example-generic" data-slide-to="2"></li>
   </ol>
+  <div class="carousel-inner" role="listbox">
+    <div class="carousel-item active">
+			<p class="carouselTitle">Arashiyama</p>
+      <img src="http://kclui.com/IVEST/ITP4208/images/BS4Carousel/carousel-1.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+			<p class="carouselTitle">Uji</p>
+      <img src="http://kclui.com/IVEST/ITP4208/images/BS4Carousel/carousel-2.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+			<p class="carouselTitle">Kiyomizu-dera</p>
+      <img src="http://kclui.com/IVEST/ITP4208/images/BS4Carousel/carousel-3.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="icon-prev" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="icon-next" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
   
  
 
@@ -56,34 +79,14 @@ while($row = mysqli_fetch_array($imageQuery))
 
 
 			
-<div class="carousel-inner" role="listbox">
-    <div class="carousel-item active">
-			<p class="carouselTitle">Arashiyama</p>
-      <img src="http://kclui.com/IVEST/ITP4208/images/BS4Carousel/carousel-1.jpg" alt="First slide">
-    </div>
-    <div class="carousel-item">
-			<p class="carouselTitle">Uji</p>
-      <img src="http://kclui.com/IVEST/ITP4208/images/BS4Carousel/carousel-2.jpg" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-			<p class="carouselTitle">Kiyomizu-dera</p>
-      <img src="http://kclui.com/IVEST/ITP4208/images/BS4Carousel/carousel-3.jpg" alt="Third slide">
-    </div>
-  </div>
+
 		
 <?php
 
 }
 ?>
 	
-  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-    <span class="icon-prev" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-    <span class="icon-next" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+  
   </div>
     <?php
 $sqlfinddiscounts = "SELECT * FROM `products` WHERE `discount` != '0'";
