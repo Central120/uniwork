0c6117b60458290f735b3a4822049ce4b444962e
Central120 <nathanwatters3212@gmail.com>
2021-04-11 22:40:23 +0100

    Update indexbackup.php

--- indexbackup.php
+++ indexbackup.php
@@ -60,7 +60,7 @@ $formatting = number_format((float)$new_price, 2, '.','');
   <img class="card-img-top" src="<?php echo $image; ?>" alt="Card image cap">
   <div class="card-body">
     <h5 class="card-title"><?php echo $product_name; ?></h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <p class="card-text"><s><?php echo $price; ?></s> <?php echo "£" . $formatting; ?></p>
   </div>
 </div>
 <?php
