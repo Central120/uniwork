7596aee267d3908897c0b7430fdff56b58bc3418
matthewcelik99 <matthewcelik99@gmail.com>
2021-04-11 19:17:48 +0100

    Update forum-comments.php

--- forum-comments.php
+++ forum-comments.php
@@ -11,14 +11,14 @@ if (isset($_SESSION['user']))
 else if(isset($_SESSION['admin']))
 {
     $session_usern = $_SESSION['admin'];
-    $modtools = "<div style='margin-bottom: 10px;margin-left: 10px;' class='btn-group' role='group' aria-label='button'>
+    $modtools = "<div style='margin-bottom: 10px;' class='btn-group' role='group' aria-label='button'>
     <form action='php/lock-post.php' method='post'>
     <input type='hidden' value='<?php echo $chosen_post;?>' name='post_id'>
     <button type='submit' title='Lock Post' class='btn btn-warning'><i class='fa fa-lock' aria-hidden='true'></i></button>
      </form>
      <form action='php/delete-post.php' method='post'>
     <input type='hidden' value='<?php echo $chosen_post;?>' name='post_id'>
-    <button type='submit' title='Delete Post' class='btn btn-danger'><i class='fa fa-trash' aria-hidden='true'></i></button>
+    <button type='submit' style='margin-left: 10px;' title='Delete Post' class='btn btn-danger'><i class='fa fa-trash' aria-hidden='true'></i></button>
      </form>
      </div>";
 }
