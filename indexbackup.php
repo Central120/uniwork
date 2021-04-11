1f850f20fab76dd24a3e3b484c9afd5ca895912b
Central120 <nathanwatters3212@gmail.com>
2021-04-11 22:31:27 +0100

    Update indexbackup.php

--- indexbackup.php
+++ indexbackup.php
@@ -28,37 +28,98 @@ session_start();
   <h2 class="mb-4">Welcome to Kerry's K9's!</h2><br>
   <p>Below are the most recent announcements.</p>
   <br>
-  <p><?php
-$sqlfinddiscounts = "SELECT * FROM `products` WHERE `discount` != '0'";
-$finddiscount = mysqli_query($conn, $sqlfinddiscounts);
-$countfinddiscount = mysqli_num_rows($finddiscount);
+  <p>
+    
+    <!-- Card -->
+<div class="card">
 
-if ($countfinddiscount == '0')
-{
-$discount_message = "We currently have no discounts on offer. Try again later!";
-$discount_style = "";
-}
-else
-{
-$discount_message = "The products we currently have discounted are:<br>";
-echo $discount_message;
-while ($rowfinddiscount = $finddiscount->fetch_assoc())
-{
-$image =$rowfinddiscount['Image'];
-$product_name = $rowfinddiscount['product_name'];
-$price = $rowfinddiscount['price'];
-$discount = $rowfinddiscount['discount'];
+<div class="view zoom overlay">
+  <img class="img-fluid w-100" src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13a.jpg" alt="Sample">
+  <h4 class="mb-0"><span class="badge badge-primary badge-pill badge-news">Sale</span></h4>
+  <a href="#!">
+    <div class="mask">
+      <img class="img-fluid w-100"
+        src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13.jpg">
+      <div class="mask rgba-black-slight"></div>
+    </div>
+  </a>
+</div>
 
-$discountcalc = $price / 100 * $discount; 
-$new_price = $price - $discountcalc; 
-$formatting = number_format((float)$new_price, 2, '.','');
+<div class="card-body text-center">
 
-$discount_style = "<img src='$image' style='height: 100px; width: 100px;'/><h5 style='display:list-item;'>$product_name - FROM: £$price, NOW: £$formatting ($discount% off!)</h5>";
+  <h5>Fantasy T-shirt</h5>
+  <p class="small text-muted text-uppercase mb-2">Shirts</p>
+  <ul class="rating">
+    <li>
+      <i class="fas fa-star fa-sm text-primary"></i>
+    </li>
+    <li>
+      <i class="fas fa-star fa-sm text-primary"></i>
+    </li>
+    <li>
+      <i class="fas fa-star fa-sm text-primary"></i>
+    </li>
+    <li>
+      <i class="fas fa-star fa-sm text-primary"></i>
+    </li>
+    <li>
+      <i class="far fa-star fa-sm text-primary"></i>
+    </li>
+  </ul>
+  <hr>
+  <h6 class="mb-3">
+    <span class="text-danger mr-1">$12.99</span>
+    <span class="text-grey"><s>$36.99</s></span>
+  </h6>
 
-echo $discount_style; 
-}
-}
-echo "<br>";
+  <button type="button" class="btn btn-primary btn-sm mr-1 mb-2">
+    <i class="fas fa-shopping-cart pr-2"></i>Add to cart
+  </button>
+  <button type="button" class="btn btn-light btn-sm mr-1 mb-2">
+    <i class="fas fa-info-circle pr-2"></i>Details
+  </button>
+  <button type="button" class="btn btn-danger btn-sm px-3 mb-2 material-tooltip-main" data-toggle="tooltip" data-placement="top" title="Add to wishlist">
+    <i class="far fa-heart"></i>
+  </button>
+
+</div>
+
+</div>
+<!-- Card -->
+    
+    
+    
+    <?php
+#$sqlfinddiscounts = "SELECT * FROM `products` WHERE `discount` != '0'";
+#$finddiscount = mysqli_query($conn, $sqlfinddiscounts);
+#$countfinddiscount = mysqli_num_rows($finddiscount);
+
+#if ($countfinddiscount == '0')
+#{
+#$discount_message = "We currently have no discounts on offer. Try again later!";
+#$discount_style = "";
+#}
+#else
+#{
+#$discount_message = "The products we currently have discounted are:<br>";
+#echo $discount_message;
+#while ($rowfinddiscount = $finddiscount->fetch_assoc())
+#{
+#$image =$rowfinddiscount['Image'];
+#$product_name = $rowfinddiscount['product_name'];
+#$price = $rowfinddiscount['price'];
+#$discount = $rowfinddiscount['discount'];
+
+#$discountcalc = $price / 100 * $discount; 
+#$new_price = $price - $discountcalc; 
+#$formatting = number_format((float)$new_price, 2, '.','');
+
+#$discount_style = "<img src='$image' style='height: 100px; width: 100px;'/><h5 style='display:list-item;'>$product_name - FROM: £$price, NOW: £$formatting ($discount% off!)</h5>";
+
+#echo $discount_style; 
+#}
+#}
+#echo "<br>";
 ?>
 </p>
 </center>
