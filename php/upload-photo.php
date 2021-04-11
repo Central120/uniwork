6e08214374046558e2468bc152dddb5461c00ed2
Central120 <nathanwatters3212@gmail.com>
2021-04-11 14:33:30 +0100

    Update upload-photo.php

--- php/upload-photo.php
+++ php/upload-photo.php
@@ -52,7 +52,7 @@ if (file_exists($target_file)) {
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") 
 {
-  $msg = "<div class='alert alert-danger alert-dismissable fade show' role='alert'><strong>Uhh ohh!</strong> Only JPG, PNG & JPEG files are allowed.$imageFileType <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+  $msg = "<div class='alert alert-danger alert-dismissable fade show' role='alert'><strong>Uhh ohh!</strong> Only JPG, PNG & JPEG files are allowed.$target_file $imageFileType <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
     <span aria-hidden='true'>&times;</span>
     </button></div>";
   $uploadOk = 0;
