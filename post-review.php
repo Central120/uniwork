e17f9ffdef4985cab26f04c53b447a2c9d301b92
shonsunny <shxnsunny29@gmail.com>
2021-04-14 22:19:39 +0100

    update post review

--- post-review.php
+++ post-review.php
@@ -29,6 +29,7 @@ $numberreview = mysqli_num_rows($searchreview);
     <title>Kerry's K9's - Reviews</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="post-review.css" rel="stylesheet" type="text/css">
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css' />
