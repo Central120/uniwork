b902c853b14c5825e4028606666759ed2caa802a
shonsunny <shxnsunny29@gmail.com>
2021-04-14 23:19:13 +0100

    Update post-review.php

--- post-review.php
+++ post-review.php
@@ -33,7 +33,11 @@ $numberreview = mysqli_num_rows($searchreview);
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css' />
-
+    <style>
+        .footer{
+            margin-top:15%!important;
+        }
+    </style>
 </head>
 <body>
 <?php include "inc/header.php"; ?>
