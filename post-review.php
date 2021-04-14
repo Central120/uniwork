346543a9caecebd32a8cd4841b3016f8b61cbe82
shonsunny <shxnsunny29@gmail.com>
2021-04-14 22:17:33 +0100

    Create post-review.php

---
+++ post-review.php
@@ -0,0 +1,81 @@
+<?php
+include_once 'inc/dbconnect.php';
+session_start();
+$current_timestamp = date('Y-m-d H:i:s');
+
+
+if (isset($_SESSION['user']))
+{
+    $session_usern = $_SESSION['user'];
+}
+else if(isset($_SESSION['admin']))
+{
+    $session_usern = $_SESSION['admin'];
+}
+else
+{
+    $session_usern = "Guest";
+}
+
+$findreview = "SELECT * from reviews where status = 'approved' order by submit_date asc";
+$searchreview = mysqli_query($conn, $findreview);
+$numberreview = mysqli_num_rows($searchreview);
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Kerry's K9's - Reviews</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css' />
+
+</head>
+<body>
+<?php include "inc/header.php"; ?>
+<div class="container-fluid" >
+    <div class="d-flex justify-content-center">
+        <center>
+            <h2 class="mb-4">Welcome to Kerry's K9's reviews!</h2>
+            <div class="txt-center">
+                <form>
+                    <div class="star_rating">
+                        <input id="star5" name="star" type="radio" value="5" class="radio-btn hide" />
+                        <label for="star5" >☆</label>
+                        <input id="star4" name="star" type="radio" value="4" class="radio-btn hide" />
+                        <label for="star4" >☆</label>
+                        <input id="star3" name="star" type="radio" value="3" class="radio-btn hide" />
+                        <label for="star3" >☆</label>
+                        <input id="star2" name="star" type="radio" value="2" class="radio-btn hide" />
+                        <label for="star2" >☆</label>
+                        <input id="star1" name="star" type="radio" value="1" class="radio-btn hide" />
+                        <label for="star1" >☆</label>
+                        <div class="clear"></div>
+                    </div>
+                </form>
+            </div>
+
+
+
+
+
+
+<?php include "inc/footer.php"; ?>
+</body>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<script>
+    $("#reviewbutton").click(function(){
+        window.location.replace("post-review");
+    });
+
+
+</script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+</html>
+
