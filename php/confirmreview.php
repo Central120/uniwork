b233fefc618b2f55c99bd3503932c06e11a0e10a
shonsunny <shxnsunny29@gmail.com>
2021-04-15 23:35:26 +0100

    Create confirmreview.php

---
+++ php/confirmreview.php
@@ -0,0 +1,39 @@
+<?php
+include_once '../inc/dbconnect.php';
+session_start();
+$current_timestamp = date('Y-m-d H:i:s');
+
+if (isset($_SESSION['user']))
+{
+    $session_usern = $_SESSION['user'];
+}
+else if(isset($_SESSION['admin']))
+{
+    $session_usern = $_SESSION['admin'];
+}
+else
+{
+    echo "<script>window.location.replace('../index');</script>";
+}
+
+$star = mysqli_real_escape_string($conn, $_POST['star']);
+echo $star;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
