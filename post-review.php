e27b0688ca5ceb390dfca61580d795297978f654
shonsunny <shxnsunny29@gmail.com>
2021-04-16 00:54:13 +0100

    Update post-review.php

--- post-review.php
+++ post-review.php
@@ -65,7 +65,7 @@ $numberreview = mysqli_num_rows($searchreview);
                 <div class="mb-3 row">
                     <label for="inputComment" class="col-sm-2 col-form-label">Review:</label>
                     <div class="col-sm-10">
-                        <input type="comments" class="form-control" id="comments" name="comments">
+                        <textarea class="form-control" id="comments" name='comments' minlength='10' rows="3"></textarea>
                     </div>
                 </div>
             <div>
