b0c138ea4d053281d4ad7daa6f147a843ca0560c
WhySoSerious4 <80232423+WhySoSerious4@users.noreply.github.com>
2021-04-15 23:00:07 +0100

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -158,7 +158,6 @@ echo "<img src='$image1' style='height: 100px; width: 100px;'/><h5 style='displa
       },
       onApprove: function(data, actions) {
         return actions.order.capture().then(function(details) {
-          alert('Transaction completed by ' + details.payer.name.given_name + '!');
           window.location.replace('php/finalise-checkout.php');
         });
       },
