80cf78b109e519149277cc4c04af472b3e8912db
shonsunny <shxnsunny29@gmail.com>
2021-04-16 01:30:42 +0100

    user permissions

---
+++ admin/php/suspend.php
@@ -0,0 +1,39 @@
+<?php
+include_once '../inc/dbconnect.php';
+session_start();
+$current_timestamp = date('Y-m-d H:i:s');
+
+if(isset($_SESSION['admin']))
+{
+    $session_usern = $_SESSION['admin'];
+}
+else
+{
+    echo "<script>window.location.replace('../index');</script>";
+}
+
+$user_id = mysqli_real_escape_string($conn, $_POST['id']);
+
+$sqlfinduser = mysqli_query($conn, "SELECT * FROM `accounts` WHERE `id` = '$user_id'");
+
+$countuser = mysqli_num_rows($sqlfinduser);
+
+if($countuser != 0){
+    $updateuser = mysqli_query($conn, "UPDATE `accounts` SET `admin_id` = '0' WHERE `id` = '$user_id'" );
+    if ($updateuser){
+        echo "<script>window.location.replace('../users');</script>";
+    }
+    else {
+        echo "<div class='alert alert-danger alert-dismissable fade show' role='alert'><strong>An error occured.</strong> The user was not suspended. <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+            <span aria-hidden='true'>&times;</span>
+          </button></div>";
+    }
+}
+else {
+    echo "<div class='alert alert-danger alert-dismissable fade show' role='alert'><strong>An error occured.</strong> The user was not found. <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+            <span aria-hidden='true'>&times;</span>
+          </button></div>";
+}
+
+
+?>
