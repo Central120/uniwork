5495c38cf7272cd6e4b48f3653180d2c869e82e9
Central120 <nathanwatters3212@gmail.com>
2021-04-17 22:35:18 +0100

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -85,9 +85,9 @@ $findcurrentbookings = mysqli_query($conn, "SELECT * FROM `bookings` WHERE `user
             }
             ?>
             
-            <div style="float: right; display: inline-block;">
+            
             <a href='#' type='button' data-toggle='modal' id='cancel_btn' data-target='<?php echo "#manage{$imageid}";?>' class='btn btn-primary'/ title='Report <?php echo $author, 's image';?>'><i class="fa fa-gavel"></i></a> </div>
-            </div>
+            
             <p class="small text-white mb-0"><?php echo $caption; ?></p>
             <div class="d-flex align-items-center justify-content-between rounded-pill bg-light px-3 py-2 mt-4">
               <p class="small mb-0"><i class="fa fa-user-o mr-2"></i><span class="font-weight-bold"><?php echo $author; ?></span></p>
