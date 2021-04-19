1c56cb31d05e34f26249fc9ec1ca73131542e147
Central120 <nathanwatters3212@gmail.com>
2021-04-19 21:38:06 +0100

    Update gallery.php

--- admin/gallery.php
+++ admin/gallery.php
@@ -97,7 +97,7 @@ else
         echo "
         
     <div class='container'>
-    <center><a href='index' class='btn btn-primary'>Back</a>
+    <center><a href='index' class='btn btn-primary'>Back</a></center>
     <div class='row'>
     
     <div class='col-md-3'>
@@ -120,7 +120,6 @@ else
     </div>
   </div>
 </div>
-</center>
     ";
     	?>
       
