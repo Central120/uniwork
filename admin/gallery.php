3cf7a31e24656febc2aca9a5e7d7395180554bd1
Central120 <nathanwatters3212@gmail.com>
2021-04-19 21:37:17 +0100

    Update gallery.php

--- admin/gallery.php
+++ admin/gallery.php
@@ -95,8 +95,9 @@ else
         <hr class="bg-dark mb-4 w-25">
 <?php
         echo "
-        <a href='index' class='btn btn-primary'>Back</a>
+        
     <div class='container'>
+    <a href='index' class='btn btn-primary'>Back</a>
     <div class='row'>
     
     <div class='col-md-3'>
